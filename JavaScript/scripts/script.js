d857439c8e5ec07ac9812cf2b0601822e95e9406
krishtopiwala <krishtopiwala1724@gmail.com>
2024-10-19 11:56:55 +0530

    add variables

---
+++ JavaScript/scripts/script.js
@@ -0,0 +1,28 @@
+// Output Statement
+
+// console.log("Jay Ranchhod");
+// document.write("Hello World");
+
+// Variables
+
+var a = 10;
+var b = 20;
+var c = 30;
+var $temp = 100;
+// var 7a = 2;
+var _k = 25;
+var camelCase = "This is camelCase variable";
+var PascalCase = "This is PascalCase variable";
+var snake_case_variable = "This is snake_case_variable";
+
+console.log(a);
+console.log($temp);
+console.log(_k);
+
+console.log(camelCase);
+console.log(PascalCase);
+console.log(snake_case_variable);
+
+// DOM
+
+document.getElementById("heading").innerText = "This is id element of DOM";
