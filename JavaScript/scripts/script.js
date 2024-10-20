4ef48eabf442a91192ff6badbab8573232546735
krishtopiwala <krishtopiwala1724@gmail.com>
2024-10-20 09:02:31 +0530

    updated commit

--- JavaScript/scripts/script.js
+++ JavaScript/scripts/script.js
@@ -10,6 +10,7 @@ var b = 20;
 var c = 30;
 var $temp = 100;
 // var 7a = 2;
+// var @temp = 5;
 var _k = 25;
 var camelCase = "This is camelCase variable";
 var PascalCase = "This is PascalCase variable";
