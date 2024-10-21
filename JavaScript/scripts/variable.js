5db2c60b637f2f1b1e606fbba09ae93b0697fdcd
krishtopiwala <krishtopiwala1724@gmail.com>
2024-10-21 11:33:15 +0530

    variable keywords commit

---
+++ JavaScript/scripts/variable.js
@@ -0,0 +1,87 @@
+// JavaScript Variable
+
+// var keyword can be redeclared and it is global scope
+
+a = 10;
+// console.log(a);
+var a;
+// var a = 20;
+
+// var keyword hoisting
+
+var x, y, z;
+x = "Smit";
+y = "Jayesh";
+
+// console.log(x);
+// console.log(y);
+
+// var globalScope = "JavaScript";
+// console.log(globalScope);
+
+// {
+//     var blockVariable = "jQuery";
+//     console.log(blockVariable);
+// }
+
+// let keyword
+
+let temp = 10;
+// console.log(temp);
+// temp1 = 20;
+
+
+
+
+// JavaScript Variable
+
+// var keyword can be redeclared and it is global scope
+
+a = 10;
+// console.log(a);
+var a;
+// var a = 20;
+
+// var keyword hoisting
+
+var x, y, z;
+x = "Smit";
+y = "Jayesh";
+
+// console.log(x);
+// console.log(y);
+
+// var globalScope = "JavaScript";
+// console.log(globalScope);
+
+// {
+//     var blockVariable = "jQuery";
+//     console.log(blockVariable);
+// }
+
+// let keyword
+
+// let temp1 = 10;
+// console.log(temp1);
+// temp1 = 20;
+
+// Let hoisting
+
+let temp1, temp2, temp3;
+
+temp1 = 10;
+temp2 = 20;
+
+// document.write(temp1 + " " + temp2);
+
+// console.log(k);
+const k = "HTML";
+console.log(k);
+
+{
+    const team1 = "India";
+    console.log(team1);
+}
+/* const variable keyword does not have hoisting.
+   it must be declared at the time of intialization
+*/
