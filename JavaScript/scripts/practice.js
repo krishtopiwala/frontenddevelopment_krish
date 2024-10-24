4bf0cfd1426eaac1f76c65b72b708146973c1f8f
krishtopiwala <krishtopiwala1724@gmail.com>
2024-10-24 17:50:00 +0530

    add operator logic

---
+++ JavaScript/scripts/practice.js
@@ -0,0 +1,45 @@
+// 1.Add two numbers input by the user
+
+// let num1 = prompt("enter number1: ");
+// let num2 = prompt("enter number2: ");
+
+// alert(`Addition is: ${Number(num1) + Number(num2)}`);
+
+// 2.Calculate the area of a rectangle
+
+let length = 10;
+let width = 50;
+
+console.log(`Area of rectangle is ${length * width}`);
+
+// 3.Convert temperature from Celsius to Fahrenheit
+
+let celsius = 27;
+let fahrenheit = ((celsius * 9) / 5) + 32;
+console.log("Fahrenheit is : ", fahrenheit);
+
+// 4.Find the remainder when one number is divided by another
+
+console.log(10 % 6);
+
+// 5.Calculate the power of a number
+
+let value = 5;
+let power = 3;
+
+console.log(`Power of a number ${value} is ${value ** power}`);
+
+// 7.Comparing Numbers [5 number]
+
+let n1 = 10;
+let n2 = 55;
+
+if (n1 > n2) {
+    console.log("n1 is greater");
+} else if (n1 < n2) {
+    console.log("n2 is greater");
+} else if (n1 == n2) {
+    console.log("n1 is equal to n2");
+} else {
+    console.log("n1 is not equal to n2");
+}
