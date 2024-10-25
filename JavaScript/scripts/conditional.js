202a70c8fc37758f5a8605f9ecc23377aca83fe2
krishtopiwala <krishtopiwala1724@gmail.com>
2024-10-25 21:24:28 +0530

    updated commit

--- JavaScript/scripts/conditional.js
+++ JavaScript/scripts/conditional.js
@@ -45,6 +45,18 @@
     } else {
         console.log("Invalid grade");
     }
+
+    let number = 35;
+
+    if (number % 5 == 0 && number % 7 == 0) {
+        console.log(`${number} is divisible by both 5 and 7`);
+    } else if (number % 5 == 0) {
+        console.log(`${number} is divisible by 5`);
+    } else if (number % 7 == 0) {
+        console.log(`${number} is divisible by 7`);
+    } else {
+        console.log(`${number} is not divisible by 5 and 7`);
+    }
 }
 
 // nested if statement
