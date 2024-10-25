a8da8571982f14458d5fe9eca61c4a831e3b0193
krishtopiwala <krishtopiwala1724@gmail.com>
2024-10-25 21:15:21 +0530

    add switch case

--- JavaScript/scripts/conditional.js
+++ JavaScript/scripts/conditional.js
@@ -66,3 +66,33 @@
         console.log("You are not Indian");
     }
 }
+
+// switch case statement
+
+let week = "Tuesday";
+
+switch (week) {
+    case "Monday":
+        console.log("Monday");
+        break;
+    case "Tuesday":
+        console.log("Tuesday");
+        break;
+    case "Wednesday":
+        console.log("Wednesday");
+        break;
+    case "Thursday":
+        console.log("Thursday");
+        break;
+    case "Friday":
+        console.log("Friday");
+        break;
+    case "Saturday":
+        console.log("Saturday");
+        break;
+    case "Sunday":
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid week day");
+}
