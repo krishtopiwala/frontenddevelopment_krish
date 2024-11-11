f2d11f43865637674dae8b29d4d58aab6f0530e8
krishtopiwala <krishtopiwala1724@gmail.com>
2024-11-11 16:35:54 +0530

    updated multiple operator question

--- JavaScript/scripts/practice.js
+++ JavaScript/scripts/practice.js
@@ -86,22 +86,5 @@ if (n1 > n2) {
 // 10. Using Multiple Operators [4 types]
 
 {
-    console.log(5 + 2 - 3);
-    console.log((10 + 20) * 10);
-
-    let a = 15;
-    let b = 700;
-    let c = 92;
-    
-    if ((a > b) && (a > c)) {
-        console.log("a is greater than b and c");
-    } else if ((b > a) && (b > c)) {
-        console.log("b is greater than a and c");
-    } else if ((c > a) && (c > b)) {
-        console.log("c is greater than a and b");
-    } else {
-        console.log("Invalid");
-    }
-
-    console.log((8 * 6) + (79 / 3));
+    console.log((5 > 3) && (-5 < 3));
 }
