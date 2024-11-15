305e0397d28397ca350fae074b48a90a91e28f57
krishtopiwala <krishtopiwala1724@gmail.com>
2024-11-15 08:36:43 +0530

    loops commit

---
+++ JavaScript/scripts/loop-practice.js
@@ -0,0 +1,27 @@
+// For Loop
+
+{
+    for (let i = 1; i <= 10; i++) {
+        console.log(i);
+    }
+}
+
+// While Loop
+
+{
+    let i = 20;
+    while (i > 0) {
+        console.log(i);
+        i--;
+    }
+}
+
+// Do While Loop
+
+{
+    let i = 5;
+    do {
+        console.log(`do while i ${i}`);
+        i++;
+    } while (i < 8);
+}
