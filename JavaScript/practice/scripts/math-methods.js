a200a6b3efb40272e4c81d92860ce56772e014bc
krishtopiwala <krishtopiwala1724@gmail.com>
2024-11-26 16:58:54 +0530

    math methods commit

---
+++ JavaScript/practice/scripts/math-methods.js
@@ -0,0 +1,70 @@
+// Math Object Methods
+
+{
+    // Q-1
+
+    let randomNumber = Math.round((Math.random() * 100) + 1);
+    console.log(randomNumber);
+}
+
+{
+    // Q-2
+
+    console.log(Math.sqrt(144));
+}
+
+{
+    // Q-3
+
+    let radius = 15;
+    let areaOfCircle = (Math.PI * Math.pow(radius, 2));
+    console.log(`Area of Circle ${Math.round(areaOfCircle).toFixed(2)}`);
+}
+
+{
+    // Q-4
+
+    let num = 365.8236;
+    console.log(Math.round(num));
+}
+
+{
+    // Q-5
+
+    console.log(Math.max(56, 214, 1, 4896, 98));
+    console.log(Math.min(56, 214, 1, 4896, 98));
+}
+
+{
+    // Q-6
+
+    console.log(Math.floor(Math.random() * 6) + 1);
+}
+
+{
+    // Q-7
+
+    console.log(Math.pow(6, 3));
+}
+
+{
+    // Q-8
+
+    let num = 2351.2365;
+    let rounded = Math.round(num * 100) / 100;
+    console.log(rounded);
+}
+
+{
+    // Q-9
+
+    let absNum = -654;
+    console.log(Math.abs(absNum));
+}
+
+{
+    // Q-10
+
+    let randomNum = Math.round(Math.random() * (5 - 1) + 1);
+    console.log(randomNum);
+}
