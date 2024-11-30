644125bf70afd21c19d781bb1bb54597bf676511
krishtopiwala <krishtopiwala1724@gmail.com>
2024-11-30 22:28:50 +0530

    updated commit

--- JavaScript/scripts/number-properties.js
+++ JavaScript/scripts/number-properties.js
@@ -70,12 +70,13 @@
 // toPrecision()
 
 {
-    console.log("=== toFixed() ===");
+    console.log("=== toPrecision() ===");
 
     let num = 3221.36528;
     console.log(num.toPrecision());
     console.log(num.toPrecision(1));
     console.log(num.toPrecision(2));
+    console.log(num.toPrecision(3));
     console.log(num.toPrecision(4));
     console.log(num.toPrecision(5));
 }
